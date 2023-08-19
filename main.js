55593a54c8865162d0320a7c7f734b5cca0145fd
Kevin <litepropageskevin@gmail.com>
2023-08-19 00:16:33 -0600

    commit inicial

---
+++ main.js
@@ -0,0 +1,97 @@
+let sound1 = new Audio('TE EXTRAÑO MASTERING.wav');
+
+playBtn1.addEventListener('click', ()=>{
+    sound1.play();
+})
+
+stopBtn1.addEventListener('click', ()=>{
+    sound1.pause();
+})
+
+
+let sound2 = new Audio('COLOMBIANA MASTERING REGGAETON.wav');
+
+playBtn2.addEventListener('click', ()=>{
+    sound2.play();
+})
+
+stopBtn2.addEventListener('click', ()=>{
+    sound2.pause();
+})
+
+
+let sound3 = new Audio('QUEMA MASTERING.wav');
+
+playBtn3.addEventListener('click', ()=>{
+    sound3.play();
+})
+
+stopBtn3.addEventListener('click', ()=>{
+    sound3.pause();
+})
+
+
+let sound10 = new Audio('LOCO Y MEXICANO MASTERING.wav');
+
+playBtn10.addEventListener('click', ()=>{
+    sound10.play();
+})
+
+stopBtn10.addEventListener('click', ()=>{
+    sound10.pause();
+})
+
+
+let sound11 = new Audio('ELLA MASTERING.wav');
+
+playBtn11.addEventListener('click', ()=>{
+    sound11.play();
+})
+
+stopBtn11.addEventListener('click', ()=>{
+    sound11.pause();
+})
+
+
+let sound12 = new Audio('MI BARRIO MASTERING.wav');
+
+playBtn12.addEventListener('click', ()=>{
+    sound12.play();
+})
+
+stopBtn12.addEventListener('click', ()=>{
+    sound12.pause();
+})
+
+
+let sound13 = new Audio('OJOS ROJOS.wav');
+
+playBtn13.addEventListener('click', ()=>{
+    sound13.play();
+})
+
+stopBtn13.addEventListener('click', ()=>{
+    sound13.pause();
+})
+
+
+let sound19 = new Audio('UN TRAGO MASTERING.wav');
+
+playBtn19.addEventListener('click', ()=>{
+    sound19.play();
+})
+
+stopBtn19.addEventListener('click', ()=>{
+    sound19.pause();
+})
+
+
+let sound28 = new Audio('NOSOTROS MASTERING.wav');
+
+playBtn28.addEventListener('click', ()=>{
+    sound28.play();
+})
+
+stopBtn28.addEventListener('click', ()=>{
+    sound28.pause();
+})
